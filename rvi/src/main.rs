2a9deba43e1ee3b6228354a887934af00b6a8cbb
Mile Davidovic <Mile.Davidovic@gmail.com>
2025-07-01 07:33:06 +0200

    No warnings.

--- rvi/src/main.rs
+++ rvi/src/main.rs
@@ -1,6 +1,6 @@
 // Usage: bmore [-acdir] [-lines] [+linenum | +/pattern] name1 name2 ...
 
-use util::{COPYRIGHT, Options};
+use util::{COPYRIGHT};
 fn main() {
     println!("copyright: {COPYRIGHT:?}");
 }
